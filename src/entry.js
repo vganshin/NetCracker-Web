0e5b5997a4de0c9dc467067d138e1fe93cc03f0e
Максим <maksim_larin@yahoo.com>
2015-11-10 03:05:43 +0300

    sadas

--- src/entry.js
+++ src/entry.js
@@ -1,39 +1,12 @@
-// import React from "react"
-// import ReactDOM from "react-dom"
-// import Navigation from "components/navigation"
-// import api from "api"
+import React from "react"
+import {render} from "react-dom"
 
-// document.api = api
-
-// ReactDOM.render(
-//   <Navigation />,
-//   document.getElementById('react-app')
-// );
-
-// import { createStore } from 'redux'
-// import todoApp from 'reducers'
-// import { addTodo } from "actions"
-
-
-// let store = createStore(todoApp)
-
-// store.dispatch(addTodo("dodod"))
-
-// console.log(store.getState().todos)
-
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from 'containers/App'
-import todoApp from 'reducers'
-
-let store = createStore(todoApp)
-
-let rootElement = document.getElementById('react-app')
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-)
+class Hello extends React.Component{
+	render(){
+		return{
+			<div> "Hello" + {this.props.name}</div>
+		}
+	}
+}
+var rootElement = document.getElementById("react-app")
+render(<Hello name="Mad Max"/>, rootElement)
