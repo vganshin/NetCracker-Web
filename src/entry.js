2c903bc850d52f7893862c2b52937235f3493916
Максим <maksim_larin@yahoo.com>
2015-11-10 23:06:21 +0300

    Страница регистрации и авторизации

--- src/entry.js
+++ src/entry.js
@@ -1,12 +1,26 @@
 import React from "react"
 import {render} from "react-dom"
+import {createStore} from "redux"
+var TimerExample = React.createClass({
+    getInitialState: function(){
+        return { elapsed: 0 };
+    },
+    componentDidMount: function(){
+        this.timer = setInterval(this.tick, 500);
+    },
+    componentWillUnmount: function(){
+        clearInterval(this.timer);
+    },
+    tick: function(){
+        this.setState({elapsed: new Date() - this.props.start});
+    },
+    render: function() {        
+        var elapsed = Math.round(this.state.elapsed / 100);
+        var seconds = (elapsed / 10).toFixed(1);  
 
-class Hello extends React.Component{
-	render(){
-		return{
-			<div> "Hello" + {this.props.name}</div>
-		}
-	}
-}
-var rootElement = document.getElementById("react-app")
-render(<Hello name="Mad Max"/>, rootElement)
+        return <p>Вы на этой странице <b>{seconds} секунд</b> </p>;
+    }
+});
+render(
+    <TimerExample start={Date.now()} />,document.getElementById("react-app")
+);
