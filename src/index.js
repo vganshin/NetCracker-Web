8476895081d6fa22872923b56de8b39d16d9c8b6
Vladislav Ganshin <ganshinv@gmail.com>
2015-11-18 14:29:44 +0300

    Template

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
-import { ReduxRouter, reduxReactRouter } from 'redux-router';
+import { ReduxRouter } from 'redux-router';
 import { Provider } from 'react-redux';
-import { createHistory } from 'history';
-import createLogger from 'redux-logger';
-import routes from 'routes'
-import reducer from 'reducers'
-
-const store = compose(
-  applyMiddleware(thunk, createLogger()),
-  reduxReactRouter({ createHistory })
-)(createStore)(reducer);
+import routes from 'routes';
+import store from 'store';
 
 class Root extends React.Component {
   render() {
